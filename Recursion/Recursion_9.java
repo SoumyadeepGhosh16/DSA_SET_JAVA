020ec6bc8936a434c57c2ad1831b14ca01a11ac4
Soumyadeep Ghosh <139796191+SoumyadeepGhosh16@users.noreply.github.com>
2024-02-26 01:15:01 +0530

    Add files via upload

---
+++ Recursion/Recursion_9.java
@@ -0,0 +1,25 @@
+public class Recursion_9 {
+//remove duplicate strings by recursion
+    public static boolean[] map=new boolean[26];
+
+    public static void removeDuplicates(String str,int idx,String newString){
+
+        if(idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar=str.charAt(idx);
+        if(map[currChar-'a']==true){
+            removeDuplicates(str, idx+1, newString);
+        }else{
+            newString+=currChar;
+            map[currChar-'a']=true;
+            removeDuplicates(str, idx+1, newString);
+        }
+    }
+    
+    public static void main(String[] args) {
+        String str="abafbvcdxxx";
+        removeDuplicates(str, 0, "");
+    }
+}
