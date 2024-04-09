2cbc4f70b8aa17d5ea76624c019cde6b23f6f18e
Soumyadeep Ghosh <139796191+SoumyadeepGhosh16@users.noreply.github.com>
2024-04-09 22:40:48 +0530

    Add files via upload

---
+++ Queue/Queue_4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Queue_4 {
+    public static void main(String[] args) {
+        
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        // 1 2 3 4 5  
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
